0ecc73f9b3cb708de1186e2a728290cabc02e117
Nicholas Reeder <nickreeder32@gmail.com>
2021-04-23 12:54:06 -0700

    add ui error handling for incorrect inputs

--- src/main.js
+++ src/main.js
@@ -16,25 +16,59 @@ $("#convert").on("click", (event) => {
   event.preventDefault();
   const amount = parseFloat($("#dollars").val());
   var currency = $("#currency-selecter option:selected").text();
-  ConverterService.getConversionRateForCurrencyWithAmount(
-    currency,
-    amount
-  ).then(function (response) {
-    showConversion(response, amount, currency);
-  });
+  console.log(currency);
+  if (!Number.isNaN(amount) && currency !== "Country Currency Code") {
+    ConverterService.getConversionRateForCurrencyWithAmount(
+      currency,
+      amount
+    ).then(function (response) {
+      showConversion(response, amount, currency);
+    });
+  } else {
+    showConversion(null, amount, currency);
+  }
+});
+$("#reset").on("click", () => {
+  location.reload();
 });
 
-const showElements = (error) => {
+$("#form").on("change", () => {
+  resetForm();
+});
+$("#dollars").on("input", () => {
+  resetForm();
+});
+
+const resetForm = () => {
+  $("#result").fadeOut();
+  $("#reset").fadeOut();
+  $("#convert").fadeIn();
+};
+
+const updateForm = () => {
   $("#result").fadeIn();
+  $("#reset").fadeIn();
+  $("#convert").fadeOut();
+};
+
+const showElements = (error) => {
+  updateForm();
   if (error) {
     $("h3").hide();
-    $("p").hide();
+    $("h4").hide();
     $(".show-errors").fadeIn();
+  } else {
+    $("h2").hide();
+    $("h3").fadeIn();
+    $("h4").fadeIn();
   }
 };
 
 const showConversion = (response, amount, currency) => {
-  if (response.result === "success") {
+  if (!response) {
+    $(".show-errors").text("Input entered in wrong format");
+    showElements(true);
+  } else if (response.result === "success") {
     $("#amount").text(amount);
     $("#conversion-rate").text(response.conversion_rate);
     $("#value").text(`${response.conversion_result} in ${currency}`);
